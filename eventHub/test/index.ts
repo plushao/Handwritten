ee6824fe448bee55e90d658033f0df84db11b843
plushao <767832499@qq.com>
2020-02-05 12:02:19 +0800

    emit test

--- eventHub/test/index.ts
+++ eventHub/test/index.ts
@@ -6,10 +6,11 @@ console.assert(eventHub instanceof Object,'eventHub 是个对象');
 
 // on emit
 let called = false;
-eventHub.on('xxx', () => {
+eventHub.on('xxx', (data) => {
     called = true;
     console.log('called:' + called);
+    console.assert(data === '测试');
 });
 
-eventHub.emit('xxx');
+eventHub.emit('xxx','测试');
 
