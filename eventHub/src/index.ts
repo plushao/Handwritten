06f6a1977155aeb5a8ab6bbb67a8e11e8ce3289c
plushao <767832499@qq.com>
2020-02-05 11:59:03 +0800

    重构 EventHub

--- eventHub/src/index.ts
+++ eventHub/src/index.ts
@@ -3,21 +3,13 @@ class EventHub {
 
     on(eventName, fn) {
         // 把fn 推进 this.cache[eventName] 数组
-        if (this.cache[eventName] === undefined) {
-            this.cache[eventName] = [];
-        }
+        this.cache[eventName] = this.cache[eventName] || [];
         this.cache[eventName].push(fn);
     }
 
     emit(eventName) {
         // 把this.cache[eventName] 数组里的 fn 全部依次调用
-        let array = this.cache[eventName];
-        if (array === undefined) {
-            array = [];
-        }
-        array.forEach(fn => {
-            fn();
-        });
+        (this.cache[eventName] || []).forEach(fn => fn());
     }
 }
 
